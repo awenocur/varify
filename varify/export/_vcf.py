eb539f5233f181717b6db9a2b15104de864b1621
Adam Wenocur <wenocur@email.chop.edu>
2014-04-09 13:05:13 -0400

    first useful call information to be added to VCF export

--- varify/export/_vcf.py
+++ varify/export/_vcf.py
@@ -5,6 +5,8 @@ from django.conf import settings
 from varify.variants.models import Variant
 import os
 import re
+from varify.samples.models import Sample
+from django.db.models import Q
 
 log = logging.getLogger(__name__)
 
@@ -25,6 +27,8 @@ class VcfExporter(BaseExporter):
         template_file = open(template_path, "r")
         template_reader = vcf.Reader(template_file)
         writer = vcf.Writer(buff, template_reader)
+        for record in template_reader:
+            print record;
         template_file.close()
 
         if request.method == 'POST':
@@ -32,7 +36,36 @@ class VcfExporter(BaseExporter):
             for item in request._stream:
                 line = re.sub('[\n\r]', '', item)
                 labels.append(line)
-                #buff.write(self, line)
+
+            allSamples = Sample.objects.get_query_set()
+            labelCriteria = None
+            for nextLabel in labels:
+                nextCriterion = Q(label=nextLabel)
+                if labelCriteria == None:
+                    labelCriteria = nextCriterion
+            else:
+                labelCriteria |= nextCriterion
+            selectedSamples = allSamples.filter(labelCriteria).select_related('results').select_related('variant')
+            for sample in selectedSamples:
+                results = sample.results.all().select_related('variant')
+                for result in results:
+                    variant = result.variant
+                    next_row = vcf.model._Record(ID=variant.id,
+                                             CHROM=variant.chr,
+                                             POS=variant.pos,
+                                             REF=variant.ref,
+                                             ALT=variant.alt,
+                                             #replace the following stubs:
+                                             QUAL=result.quality, FILTER=None,
+                                             INFO=None, FORMAT='GT',
+                                             sample_indexes={sample.label:0},
+                                             samples=[None])
+                    row_call_format = vcf.model.make_calldata_tuple(['GT'])
+                    row_call_format._types.append('String')
+                    next_row_call_values = [result.genotype.label.encode('ascii', errors='backslashreplace')]
+                    next_row.samples = [vcf.model._Call(next_row, sample.label, row_call_format(*next_row_call_values))]
+                    writer.write_record(next_row)
+
         else:
             for i, row_gen in enumerate(self.read(iterable,
                                                   *args, **kwargs)):

4585c1c7687c538c4d47a84a71ccb64d6ca737c4
Adam Wenocur <wenocur@email.chop.edu>
2014-07-09 15:12:26 -0400

    added test for sample subset

--- tests/cases/export/tests.py
+++ tests/cases/export/tests.py
@@ -22,6 +22,18 @@ class QueueTestCase(TransactionTestCase):
 
 @override_settings(VARIFY_SAMPLE_DIRS=SAMPLE_DIRS)
 class SampleLoadTestCase(QueueTestCase):
+    @staticmethod
+    def prepare_request(params):
+        json = dumps(params)
+
+        # Create a nonsensical request, just so that the exporter can function.
+        request = http.HttpRequest()
+        request._stream = StringIO(json)
+        request.META['SERVER_NAME'] = 'test'
+        request.META['SERVER_PORT'] = 0
+        request.method = 'POST'
+        return request
+
     def test_pipeline(self):
         # Immediately validates and creates a sample
         from django.core import management
@@ -35,21 +47,25 @@ class SampleLoadTestCase(QueueTestCase):
         worker1.work(burst=True)
         worker2.work(burst=True)
 
+        exporter = VcfExporter()
+
+        # This first test is for 3 samples, with a range on one chromosome.
         test_params = {'ranges': [{'start': 1, 'end': 144000000, 'chrom': 1}],
                        'samples': ['NA12891', 'NA12892', 'NA12878']}
-
-        json = dumps(test_params)
-
-        # Create a nonsensical request, just so that the exporter can function.
-        request = http.HttpRequest()
-        request._stream = StringIO(json)
-        request.META['SERVER_NAME'] = 'test'
-        request.META['SERVER_PORT'] = 0
-        request.method = 'POST'
-
-        exporter = VcfExporter()
+        request = SampleLoadTestCase.prepare_request(test_params)
         buff = exporter.write(None, request = request)
         hash = hashlib.md5(buff.getvalue()[-500:])
         buff.close()
 
         self.assertEqual(hash.hexdigest(), '990fe158f2e2390a8c3bd0d673ed40d1')
+
+        # This second test is for 2 samples, to check that the remaining one
+        # is indeed being excluded.
+        test_params = {'ranges': [{'start': 1, 'end': 144000000, 'chrom': 1}],
+                       'samples': ['NA12891', 'NA12892']}
+        request = SampleLoadTestCase.prepare_request(test_params)
+        buff = exporter.write(None, request = request)
+        hash = hashlib.md5(buff.getvalue()[-500:])
+        buff.close()
+
+        self.assertEqual(hash.hexdigest(), '50dd077b5c8f55366f625e44beebf203')

ddff7edd17a2ca5102f89e24aee6d00aae5f1bae
Adam Wenocur <wenocur@email.chop.edu>
2014-07-14 16:15:04 -0400

    updated VCF exporter test to import common dependency

--- tests/cases/export/tests.py
+++ tests/cases/export/tests.py
@@ -4,21 +4,12 @@ from StringIO import StringIO
 from json import dumps
 from django import http
 from django.test.utils import override_settings
-from django.test import TransactionTestCase
-from django.core.cache import cache
-from django_rq import get_worker, get_queue, get_connection
-from rq.queue import get_failed_queue
+from django_rq import get_worker
 from varify.export._vcf import VcfExporter
+from ..base import QueueTestCase
 
 TESTS_DIR = os.path.join(os.path.dirname(__file__), '../..')
 SAMPLE_DIRS = [os.path.join(TESTS_DIR, 'samples')]
-
-class QueueTestCase(TransactionTestCase):
-    def setUp(self):
-        cache.clear()
-        get_queue('variants').empty()
-        get_queue('default').empty()
-        get_failed_queue(get_connection()).empty()
 
 @override_settings(VARIFY_SAMPLE_DIRS=SAMPLE_DIRS)
 class SampleLoadTestCase(QueueTestCase):

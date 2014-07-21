2e998496b8059d7ef2a485ec5513dc5eb4d12a00
Adam Wenocur <wenocur@email.chop.edu>
2014-07-21 17:19:55 -0400

    tests now work

--- tests/cases/dummy1/tests.py
+++ tests/cases/dummy1/tests.py
@@ -1,7 +1,13 @@
+import os
 from django.core.cache import cache
 from django.test import TestCase
 from django_rq import get_queue, get_connection
 from rq.queue import get_failed_queue
+from django.test.utils import override_settings
+from django_rq import get_worker
+
+TESTS_DIR = os.path.join(os.path.dirname(__file__), '../..')
+SAMPLE_DIRS = [os.path.join(TESTS_DIR, 'samples')]
 
 @override_settings(VARIFY_SAMPLE_DIRS=SAMPLE_DIRS)
 class dummyTestCase1(TestCase):
